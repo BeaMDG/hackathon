c8109a1c9b85ee0ad842eadb8d0da77d4d19da9e
NellyKaykay <elelyonexclusive@gmail.com>
2023-07-20 20:02:08 +0200

    creating text component

--- html/react/src/components/molecules/DuckInfo.jsx
+++ html/react/src/components/molecules/DuckInfo.jsx
@@ -3,11 +3,19 @@ import Image from '../atoms/Image';
 
 const DuckInfo = () => {
   return (
-    <div>
-      <p>Sabías que... El 10% del precio de cualquier producto va destinado a las reservas protegidas de patos</p>
-      <Image src="ruta_de_la_imagen1.jpg" alt="Patos protegidos" />
-      <p>Ayúdanos junto a la Organización WWF a mejorar tus colecciones de patos y a mejorar la vida de ellos!!</p>
+    <section>
+    <div className='main-text'>
+      <h1>Sabías que...</h1>
+    <p> El 10% del precio de cualquier producto va destinado a las reservas protegidas de patos</p>
     </div>
+    <div className='main-image'>
+    <Image src="ruta_de_la_imagen1.jpg" alt="Patos protegidos" />
+    </div>
+      <div>
+      <p>Ayúdanos junto a la Organización WWF a mejorar tus colecciones de patos y a mejorar la vida de ellos!!</p>
+      </div> 
+
+    </section>
   );
 };
 
--- html/react/src/pages/landingPage.jsx
+++ html/react/src/pages/landingPage.jsx
@@ -3,9 +3,9 @@ import CTA from '../components/organisms/CTA';
 
 const landingPage = () => {
     return (
-      <div>
+      
              <CTA />
-      </div>
+     
     );
   };
   
