762c1efa7392ba7d2cff45d4fbc66892fafcf557
NellyKaykay <elelyonexclusive@gmail.com>
2023-07-20 22:43:27 +0200

    resolving merge conflict

--- html/react/src/components/atoms/button/Btn.jsx
+++ html/react/src/components/atoms/button/Btn.jsx
@@ -1,9 +1,6 @@
 import './Btn.css'
 
 function Btn(button) {
-import React from 'react';
-
-const Button = ({ onClick, children }) => {
   return (
     <button className={button.className}>{ button.content }</button>
   )
