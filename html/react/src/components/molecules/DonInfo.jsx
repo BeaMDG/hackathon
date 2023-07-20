06aad2d65b28a1ea8b19c3bac4a900a36db8bfd8
NellyKaykay <elelyonexclusive@gmail.com>
2023-07-20 20:20:20 +0200

    creating doninfo  component

---
+++ html/react/src/components/molecules/DonInfo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Image from '../atoms/Image';
+
+const DonInfo = () => {
+  return (
+    <div>
+      <Image src="src/assets/image-dinfo/logo.png" alt="Logo" />
+      <Image src="src/assets/image-dinfo/www.png" alt="WWW" />
+    </div>
+  );
+};
+
+export default DonInfo;
