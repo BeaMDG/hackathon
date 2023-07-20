a250acd41b19d40da382cb101d144c8629db4216
NellyKaykay <elelyonexclusive@gmail.com>
2023-07-20 20:08:29 +0200

    modifying duckinfo  component

--- html/react/src/components/molecules/DuckInfo.jsx
+++ html/react/src/components/molecules/DuckInfo.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
+import Text from '../atoms/Text';
 import Image from '../atoms/Image';
 
 const DuckInfo = () => {
   return (
-    <section>
-    <div className='main-text'>
-      <h1>Sabías que...</h1>
-    <p> El 10% del precio de cualquier producto va destinado a las reservas protegidas de patos</p>
+    <div>
+      <Text>
+        Sabias que...El 10% del precio de cualquier producto va destinado a las reservas protegidas de los patos.
+      </Text>
+      <Image src="src/assets/image-dinfo/imageduckinfo.png" alt="Duck Info" />
+      <Text>
+        Ayudanos junto a la Organizacion WWF a aumentar tu coleccion de patos y a mejorar la vida de ellos!!
+      </Text>
     </div>
-    <div className='main-image'>
-    <Image src="ruta_de_la_imagen1.jpg" alt="Patos protegidos" />
-    </div>
-      <div>
-      <p>Ayúdanos junto a la Organización WWF a mejorar tus colecciones de patos y a mejorar la vida de ellos!!</p>
-      </div> 
-
-    </section>
   );
 };
 
